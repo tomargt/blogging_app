124e378bb7b20a04867f424e2666fe6bb2a9e48c
rails <gtomar@grepruby.com>
2017-10-24 15:42:38 +0530

    modified

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   before_action :current_blog, only: [:edit, :update, :publish, :archive, :destroy]
   
   def index
