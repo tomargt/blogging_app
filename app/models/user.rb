84b85715fab044ffef5ec9dc8bcf36d8be59405c
rails <gtomar@grepruby.com>
2017-10-30 16:54:26 +0530

    added username for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	
+	validates_presence_of :username
+  validates_uniqueness_of :username
 
   has_many :blogs
 end
